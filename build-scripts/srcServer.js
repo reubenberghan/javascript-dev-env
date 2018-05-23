af9c20fffe9f8b1b0f08951bc6d8d081014f7ef2
Reuben Berghan <reuben.berghan@gmail.com>
2018-05-23 20:39:36 +1200

    Added dev server using express

---
+++ build-scripts/srcServer.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import path from 'path';
+import open from 'open';
+import webpack from 'webpack';
+import config from '../webpack.config.dev';
+
+const PORT = 3000;
+
+const app = express();
+const compiler = webpack(config);
+
+app.use(require('webpack-dev-middleware')(compiler, { publicPath: config.output.publicPath }));
+
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, '../src/index.html')));
+
+app.listen(PORT, err => err ? console.log(err) : open(`http://localhost:${PORT}`));
