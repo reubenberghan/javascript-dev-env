040d5ec6988e616433acda9d5c5254807ca7b546
Reuben Berghan <reuben.berghan@gmail.com>
2018-05-26 18:38:17 +1200

    updated demo script

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 import './index.css';
 
-function helloWorld() { console.log('hello') };
+function helloWorld() { console.log('hello world!') };
 
 helloWorld();
+
+const h1 = document.createElement('h1');
+
+h1.innerHTML = 'Hello World';
+
+document.body.appendChild(h1);
