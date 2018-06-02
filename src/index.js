f380daf8f8a8de8bde0db2deacde266f2f91166a
Reuben Berghan <reuben.berghan@gmail.com>
2018-06-02 18:21:30 +1200

    added wrapper div with id for each recipe

--- src/index.js
+++ src/index.js
@@ -13,15 +13,17 @@ getRecipes()
     const div = document.createElement('div');
 
     let divBody = data.reduce((recipeHtml, recipe) => {
-      return recipeHtml += `<h2>${ recipe.title }</h2>
-      <h3><u>Ingredients</u></h3>
-      <ul>
-        ${ recipe.ingredients.reduce((ingredientHtml, ingredient) => ingredientHtml += `<li>${ ingredient }</li>`, ``) }
-      </ul>
-      <h3><u>Steps</u></h3>
-      <ol>
-        ${ recipe.steps.reduce((stepHtml, step) => stepHtml += `<li>${ step }</li>`, ``) }
-      </ol>
+      return recipeHtml += `<div id="${ recipe.id }">
+        <h2>${ recipe.title }</h2>
+        <h3><u>Ingredients</u></h3>
+        <ul>
+          ${ recipe.ingredients.reduce((ingredientHtml, ingredient) => ingredientHtml += `<li>${ ingredient }</li>`, ``) }
+        </ul>
+        <h3><u>Steps</u></h3>
+        <ol>
+          ${ recipe.steps.reduce((stepHtml, step) => stepHtml += `<li>${ step }</li>`, ``) }
+        </ol>
+      </div>
       `;
     }, ``);
 
