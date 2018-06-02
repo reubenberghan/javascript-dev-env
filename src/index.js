80a6249aa979210813370d5f35c637eb296c9392
Reuben Berghan <reuben.berghan@gmail.com>
2018-06-02 18:20:29 +1200

    generate recipes html from api call

--- src/index.js
+++ src/index.js
@@ -10,12 +10,24 @@ document.body.appendChild(el);
 
 getRecipes()
   .then(data => {
-    // const div = document.createElement('div');
-    // let divBody = '';
+    const div = document.createElement('div');
 
-    // data.reduce((recipeHtml, recipe) => {
+    let divBody = data.reduce((recipeHtml, recipe) => {
+      return recipeHtml += `<h2>${ recipe.title }</h2>
+      <h3><u>Ingredients</u></h3>
+      <ul>
+        ${ recipe.ingredients.reduce((ingredientHtml, ingredient) => ingredientHtml += `<li>${ ingredient }</li>`, ``) }
+      </ul>
+      <h3><u>Steps</u></h3>
+      <ol>
+        ${ recipe.steps.reduce((stepHtml, step) => stepHtml += `<li>${ step }</li>`, ``) }
+      </ol>
+      `;
+    }, ``);
 
-    // }, divBody);
-    console.log(data); // eslint-disable-line no-console
+    div.setAttribute('id', 'recipes');
+    document.body.appendChild(div);
+
+    document.getElementById('recipes').innerHTML = divBody;
   })
   .catch(err => console.log(err)); // eslint-disable-line no-console
